e0f8690c227d517795aaa203d25ca9b774910370
arunwrks <arun.engg.tcs@gmail.com>
2016-05-21 18:24:56 +0530

    REST/MVC EndPoints Time Profiling using SpringMVC/Boot

---
+++ src/main/java/com/cubestacklabs/restendpointtimeprofiler/AppConfig.java
@@ -0,0 +1,21 @@
+package com.cubestacklabs.restendpointtimeprofiler;
+
+import com.cubestacklabs.restendpointtimeprofiler.log.RequestLoggerHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan("com.cubestacklabs.arrow")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private RequestLoggerHandlerInterceptor requestLoggerHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggerHandlerInterceptor);
+    }
+}
